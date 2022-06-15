8c3f45982255474e2d25b75d69317383b5790604
Özcan Hoştan <77676729+ozcanhostan@users.noreply.github.com>
2022-06-15 19:50:18 +0300

    Web Site Kodlaması (Ön yüz)
    Afrikalı çocuklara yardım temalı ön yüz kodlaması

---
+++ Afrikalı Çocuklar (Web Sitesi)/app.js
@@ -0,0 +1,15 @@
+function kopyala(){
+    var metin = document.getElementById("metin");
+    metin.select();
+    document.execCommand("copy");
+}
+function kopyala1(){
+    var metin1 = document.getElementById("metin1");
+    metin1.select();
+    document.execCommand("copy");
+} 
+function kopyala2(){
+    var metin2 = document.getElementById("metin2");
+    metin2.select();
+    document.execCommand("copy");
+} 
